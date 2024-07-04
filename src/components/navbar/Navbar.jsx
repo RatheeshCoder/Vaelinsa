5b6867bb33b40cb53127e6ad04d105b4bbb95e81
Ratheesh R <ratheeshraju2003@gmail.com>
2024-07-04 21:07:18 +0530

    installed styled compound

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import * as Style from './Style'
+
+const Navbar = () => {
+  return (
+    <Style.Con>
+        
+    </Style.Con>
+  )
+}
+
+export default Navbar
---
+++ src/components/navbar/Style.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+
+export const Con = styled.section`
+width: 100%;
+height: 100%;
+
+`;
